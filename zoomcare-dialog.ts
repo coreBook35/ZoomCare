07df263a145400516e55553b84b2cba144cd7803
coreBook35 <143210793+coreBook35@users.noreply.github.com>
2023-08-25 17:34:44 -0700

    Create zoomcare-dialog.ts

---
+++ zoomcare-dialog.ts
@@ -0,0 +1,5 @@
+“You must not take all the credit to yourself, doctor. Lucy’s state this morning is due in part to me.”
+
+“How you do mean, ma’am?” asked the Professor.
+
+“Well, I was anxious about the dear child in the night, and went into her room. She was sleeping soundly—so soundly that even my coming did not wake her. But the room was awfully stuffy. There were a lot of those horrible, strong-smelling flowers about everywhere, and she had actually a bunch of them round her neck. I feared that the heavy odour would be too much for the dear child in her weak state, so I took them all away and opened a bit of the window to let in a little fresh air. You will be pleased with her, I am sure.”
